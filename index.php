3667314d5b360ddeffa5f96d73448264c3f5945b
natnarin6642 <71230118+natnarin6642@users.noreply.github.com>
2020-09-14 01:56:06 +0700

    Add files via upload
     Responsive Grid System

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    </head>
+    <body>
+      
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 
+            <div class="container">
+                <div class="row">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link text-uppercase" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-uppercase" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-uppercase" href="#">TopTen</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-uppercase" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+                </div>
+            </div>
+</nav>
+        
+      
+       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+   
+  </ol>
+  <div class="carousel-inner">
+    <!--<div class="carousel-item ">
+        <img src="images/slides/1.jpg" class="d-block w-100" alt="">
+    </div> -->
+    <div class="carousel-item active">
+        <img src="images/slides/2.jpg" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item">
+        <img src="images/slides/3.jpg" class="d-block w-100" alt="">
+    </div>
+          <div class="carousel-item">
+        <img src="images/slides/4.jpg" class="d-block w-100" alt="">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> 
+        
+   
+        <div style="margin-top: 10px;">
+            
+  <div class="shadow p-3  rounded bg-light text-dark text-center text-uppercase h2">Top 10 Knowledge Websites</div>      
+        </div>
+        
+        
+        <div class="container">
+            <div class="row" style="margin-top: 30px;">
+                
+               <div class="col-12 col-md-6 col-xl-4">    
+                  <div class="card">
+                    <img src="images/thumbnails/5.jpg" class="card-img-top">
+                       <div class="card-body">
+                         <h5 class="card-title">
+                            <a href="TopTenWeb.php">Top 10 Websites in the World</a>
+                         </h5>      
+                       </div>
+                  </div>
+              </div> 
+               
+              <div class="col-12 col-md-6 col-xl-4">    
+                  <div class="card">
+                    <img src="images/thumbnails/6.jpg" class="card-img-top">
+                       <div class="card-body">
+                         <h5 class="card-title">
+                            <a href="TopTenCar.php">Top 10 best-selling cars</a>
+                         </h5>      
+                       </div>
+                  </div>
+              </div>  
+                
+                <div class="col-12 col-md-6 col-xl-4">    
+                  <div class="card">
+                    <img src="images/thumbnails/7.jpg" class="card-img-top">
+                       <div class="card-body">
+                         <h5 class="card-title">
+                            <a href="TopTenIsland.php">10 largest islands in the world</a>
+                         </h5>      
+                       </div>
+                  </div>
+              </div>
+                
+                
+                <div class="col-12 col-md-6 col-xl-4" style="margin-top: 20px;">    
+                  <div class="card">
+                    <img src="images/thumbnails/8.jpg" class="card-img-top">
+                       <div class="card-body">
+                           <b><p class="card-title" style="font-size: 17px;">
+                            <a href="TopTenWaterFall.php">The 10 tallest waterfalls in the world</a>
+                               </p> </b>     
+                       </div>
+                  </div>
+                </div>
+             
+                 <div class="col-12 col-md-6 col-xl-4" style="margin-top: 20px;">    
+                  <div class="card">
+                    <img src="images/thumbnails/9.jpg" class="card-img-top">
+                       <div class="card-body">
+                         <h5 class="card-title">
+                            <a href="TopTenRiver.php">The 10 Longest Rivers</a>
+                         </h5>      
+                       </div>
+                  </div>
+                </div>
+                
+                 <div class="col-12 col-md-6 col-xl-4" style="margin-top: 20px;">    
+                  <div class="card">
+                    <img src="images/thumbnails/10.jpg" class="card-img-top">
+                       <div class="card-body">
+                         <h5 class="card-title">
+                            <a href="TopTenEpidemics.php">The 10 Deadliest Epidemics</a>
+                         </h5>      
+                       </div>
+                  </div>
+                </div>
+                
+           </div>
+        </div>
+        
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" ></script>       
+    </body>
+</html>
